cb3d7c12077a74711aad14d4b739069c9fade097
Bloodcast69 <gemza.damian@gmail.com>
2018-05-17 21:09:43 +0200

    Strona glowna, strona magazynu, dodawanie produktu

---
+++ front/src/app/components/add-product/add-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.css']
+})
+
+export class AddProductComponent implements OnInit {
+
+  ngOnInit() {
+    
+  }
+}
